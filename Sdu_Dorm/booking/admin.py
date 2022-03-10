d6a06bf29fa9454987c2f52dbbe3d0f33d454237
Adilkhan <timuruly765@gmail.com>
2022-03-10 21:39:22 +0600

    commit

--- Sdu_Dorm/booking/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Users, Block, Floor, Room
-# Register your models here.
-
-admin.site.register(Users)
-admin.site.register(Block)
-admin.site.register(Floor)
-admin.site.register(Room)
